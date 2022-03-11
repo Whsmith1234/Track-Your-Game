f0ebc52fd733c42cf8104fff302ab15c83fdeae7
Acme Pi <wjpra@protonmail.com>
2022-03-11 18:32:06 -0500

    Init

---
+++ sketch.js
@@ -0,0 +1,203 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+KNN Classification on Webcam Images with poseNet. Built with p5.js
+=== */
+let video;
+var x = 299;
+// Create a KNN classifier
+const knnClassifier = ml5.KNNClassifier();
+let poseNet;
+let poses = [];
+
+function setup() {
+  const canvas = createCanvas(640, 480);
+  canvas.parent('videoContainer');
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+var xstar = 100;
+var ystar = 0;
+var score =0;
+function draw() {
+  ystar+=5;
+  if(ystar>height){
+    ystar=0;
+    
+    if(x-50<xstar&&x+50>xstar){
+      score++;
+    }
+    xstar=random(0,width);
+  }
+  image(video, 0, 0, width, height);
+  background(51);
+  ellipse(x, 400, 50,50)
+  fill(255,255,100);
+  star(xstar, ystar, 10, 20, 5);
+  text(score,10,10,score);
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+}
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+function modelReady(){
+  select('#status').html('model Loaded')
+}
+
+// Add the current frame from the video to the classifier
+function addExample(label) {
+  // Convert poses results to a 2d array [[score0, x0, y0],...,[score16, x16, y16]]
+  const poseArray = poses[0].pose.keypoints.map(p => [p.score, p.position.x, p.position.y]);
+
+  // Add an example with a label to the classifier
+  knnClassifier.addExample(poseArray, label);
+  updateCounts();
+}
+
+// Predict the current frame.
+function classify() {
+  // Get the total number of labels from knnClassifier
+  const numLabels = knnClassifier.getNumLabels();
+  if (numLabels <= 0) {
+    console.error('There is no examples in any label');
+    return;
+  }
+  // Convert poses results to a 2d array [[score0, x0, y0],...,[score16, x16, y16]]
+  const poseArray = poses[0].pose.keypoints.map(p => [p.score, p.position.x, p.position.y]);
+
+  // Use knnClassifier to classify which label do these features belong to
+  // You can pass in a callback function `gotResults` to knnClassifier.classify function
+  knnClassifier.classify(poseArray, gotResults);
+}
+
+// A util function to create UI buttons
+function createButtons() {
+  // When the A button is pressed, add the current frame
+  // from the video with a label of "A" to the classifier
+  buttonA = select('#addClassA');
+  buttonA.mousePressed(function() {
+    addExample('A');
+  });
+
+  // When the B button is pressed, add the current frame
+  // from the video with a label of "B" to the classifier
+  buttonB = select('#addClassB');
+  buttonB.mousePressed(function() {
+    addExample('B');
+  });
+
+  // Reset buttons
+  resetBtnA = select('#resetA');
+  resetBtnA.mousePressed(function() {
+    clearLabel('A');
+  });
+	
+  resetBtnB = select('#resetB');
+  resetBtnB.mousePressed(function() {
+    clearLabel('B');
+  });
+
+  // Predict button
+  buttonPredict = select('#buttonPredict');
+  buttonPredict.mousePressed(classify);
+
+  // Clear all classes button
+  buttonClearAll = select('#clearAll');
+  buttonClearAll.mousePressed(clearAllLabels);
+}
+
+// Show the results
+function gotResults(err, result) {
+  // Display any error
+  if (err) {
+    console.error(err);
+  }
+
+  if (result.confidencesByLabel) {
+    const confidences = result.confidencesByLabel;
+    // result.label is the label that has the highest confidence
+    if (result.label) {
+      select('#result').html(result.label);
+      select('#confidence').html(`${confidences[result.label] * 100} %`);
+    }
+
+    select('#confidenceA').html(`${confidences['A'] ? confidences['A'] * 100 : 0} %`);
+    select('#confidenceB').html(`${confidences['B'] ? confidences['B'] * 100 : 0} %`);
+  }
+
+  classify();
+}
+
+// Update the example count for each label	
+function updateCounts() {
+  const counts = knnClassifier.getCountByLabel();
+
+  select('#exampleA').html(counts['A'] || 0);
+  select('#exampleB').html(counts['B'] || 0);
+}
+
+// Clear the examples in one label
+function clearLabel(classLabel) {
+  knnClassifier.clearLabel(classLabel);
+  updateCounts();
+}
+
+// Clear all the examples in all labels
+function clearAllLabels() {
+  knnClassifier.clearAllLabels();
+  updateCounts();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+
+
+        stroke(255, 255, 255);
+        var x1 = pose.keypoints[1].position.x;
+        var x2 = pose.keypoints[2].position.x;
+        var y1 = pose.keypoints[1].position.y;
+        var y2 = pose.keypoints[2].position.y;
+        var m = (y1-y2)/(x2-x1);
+        x+=m;
+        if (x > 640) x = 0;
+        if (x<0) x = 600;
+        line(x1,y1,x2,y2);
+      }
+    }
+  }
+
